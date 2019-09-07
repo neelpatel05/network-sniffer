7bd6881dc50eb16173eab56db3bfaff2ea4cb6ae
Neel Patel <neelpatel@wireless-10-105-205-11.umd.edu>
2019-09-07 11:45:18 -0400

    formats mac address

--- network-sniffer.py
+++ network-sniffer.py
@@ -2,8 +2,11 @@ import socket
 import struct
 import textwrap
 
-#unpack ethernet frame
 def ethernet_frame(data):
     destination_mac, source_mac, protocol = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(destination_mac), get_mac_addr(source_mac), socket.htons(protocol), data[14:]
 
+def get_mac_addr(bytes_addr):
+    bytes_string = map("{:02x}".format, bytes_addr)
+    mac_addr = ':'.join(bytes_addr).upper()
+    return mac_addr
